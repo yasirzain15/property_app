f84f8cc73fb6f023806ac2218f23c50dcdf1f246
Yasir Zain <152737441+yasirzain15@users.noreply.github.com>
2026-02-05 20:23:30 +0500

    project done

--- lib/Core/Services/api_service.dart
+++ lib/Core/Services/api_service.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get/get_connect/connect.dart';
 import 'package:get_storage/get_storage.dart';
 
 class ApiService extends GetConnect {
--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
 
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Rent Pay',
+      title: 'Property App',
 
       /// 🌍 REQUIRED FOR Directionality SAFETY
       locale: const Locale('en', 'US'),
